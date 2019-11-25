0d0a1c16d98e4bb6f9c367acc5aeda9655de998e
lalfaya27 <54558507+lalfaya27@users.noreply.github.com>
2019-11-24 22:23:42 -0300

    Add files via upload

--- js/cart.js
+++ js/cart.js
@@ -9,24 +9,25 @@ const BANKING_PAYMENT = "Transferencia bancaria";
 let ERROR_MSG = "Ha habido un error :(, verifica qué pasó.";
 let PERCENTAGE_SYMBOL = '%';
 let array = [];
-var cart= [];
+var carro = [];
 
 //Función que se utiliza para actualizar los costos de publicación
 function updateTotalCosts() {
-    
-    let totalHTML=document.getElementById("totalCost");
-    total=(subtotal*(1+shippingPercentage)).toFixed(2);
 
-    totalHTML.innerHTML= " " + "Costo Total:"+ productCurrency + total;
+    let totalHTML = document.getElementById("totalCost");
+    total = (subtotal * (1 + shippingPercentage)).toFixed(2);
+
+    totalHTML.innerHTML = " " + "Costo Total:" + " " + productCurrency + total;
 }
 
 function updateShippingCost() {
 
-    let shippingHTML=document.getElementById ("shippingCost");
-    shipping=(subtotal*shippingPercentage).toFixed(2);
+    let shippingHTML = document.getElementById("shippingCost");
+    shipping = (subtotal * shippingPercentage).toFixed(2);
 
-    shippingHTML.innerHTML= " " + "Costo Envío:" + productCurrency + shipping;
+    shippingHTML.innerHTML = " " + "Costo Envío:" + " " + productCurrency + shipping;
 }
+
 
 //función que muestra producto a comprar
 function showProductsBuy(array) {
@@ -34,45 +35,41 @@ function showProductsBuy(array) {
     for (let i = 0; i < array.length; i++) {
         let article = array[i];
 
-        productCount=article.count
-        productPrice=article.unitCost
+        productCount = article.count
+        productPrice = article.unitCost
 
         htmlContentToAppend += `
-        <a class="list-group-item list-group-item-action">
-        <div class="row">
-            <div class="col-3">
-                <img src= "${article.src}" class="img-thumbnail">
-            </div>
-        <div class="col">
-                <div class="d-flex w-100 justify-content-between">
-                        <h4 class="text-center">`+ article.name +`</h4>
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src= "${article.src}" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="text-center">`+ article.name + `</h4>
                     </div>
                     <br>
-                    <p class="mb-1"> Precio: `+ article.currency + " "  + article.unitCost + `</p>
-                      <div class="row container p-0 m-0"></div>
-                        <div class="col">
-                          <p class="text-center" id="sortByPrice">Cantidad de Artículos a Comprar:</p>
-                        </div>
-                        <div class="col">
-                          <input class="form-control" type="number" placeholder="num" id="rangeFilterNumber" value=`+ article.count +`>
-                        </div>
-                        </div
+                    <p class="mb-1"> Precio: `+ article.currency + " " + article.unitCost + `</p>
+                    <div class="row container p-0 m-4"></div>
+                        <p class="d-flex w-100 justify-content-between" id="sortByPrice">Cantidad de Artículos a Comprar:</p>
+                        <input class="form-control" type="number" style='width:200px; height:25px' placeholder="num" id="numeroArticulos" value=`+ article.count + `>
+                    </div>
                 </div>
             </div>
-        </a>
+        </div>
         `
 
 
         document.getElementById("productoComprar").innerHTML = htmlContentToAppend;
 
     }
-} 
+}
 
 function updateSubtotal() {
-    let subtotalHTML=document.getElementById("subtotalCost");
-    subtotal=(productPrice*productCount);
+    let subtotalHTML = document.getElementById("subtotalCost");
+    subtotal = (productPrice * productCount).toFixed(2);
 
-    subtotalHTML.innerHTML= "Costo Subtotal:"+ " " + productCurrency + subtotal;
+    subtotalHTML.innerHTML = " " + "Costo Subtotal:" + " " + productCurrency + subtotal;
 }
 
 function showPaymentTypeNotSelected() {
@@ -83,29 +80,52 @@ function hidePaymentTypeNotSelected() {
 
 }
 
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(CART_INFO_URL).then(function (resultObj) {
         if (resultObj.status === "ok") {
-            cart=resultObj.data;
+            carro = resultObj.data;
 
-            showProductsBuy(cart.articles);
-            updateSubtotal(cart.articles);
+            showProductsBuy(carro.articles);
+            updateSubtotal(carro.articles);
             updateTotalCosts();
             updateShippingCost();
-
-
         }
-                
-        document.getElementById("rangeFilterNumber").addEventListener("change", function (){
-            productCount= parseInt(document.getElementById('rangeFilterNumber').value)
-            updateSubtotal(cart.artcicles);
+
+        document.getElementById("numeroArticulos").addEventListener("change", function () {
+            productCount = parseInt(document.getElementById('numeroArticulos').value)
+            updateSubtotal(carro.artcicles);
             updateTotalCosts();
             updateShippingCost()
-        })
+        });
 
+        document.getElementById("premiumRadio").addEventListener("change", function () {
+            shippingPercentage = 0.15;
+            updateShippingCost();
+            updateTotalCosts();
+        });
 
+        document.getElementById("expressRadio").addEventListener("change", function () {
+            shippingPercentage = 0.07;
+            updateShippingCost();
+            updateTotalCosts();
+        });
+
+        document.getElementById("standardRadio").addEventListener("change", function () {
+            shippingPercentage = 0.05;
+            updateShippingCost();
+            updateTotalCosts();
+        });
     });
-})
+});
+document.addEventListener('DOMContentLoaded', function(){
+    let form=document.getElementById('fcompra');
+     form.addEventListener("submit", function(evento) {
+            evento.preventDefault()
+    });
+    if (document.getElementById("creditCardPaymentRadio").checked==true);
+    document.getElementById("bankingRadio").hidden==true;
+});
